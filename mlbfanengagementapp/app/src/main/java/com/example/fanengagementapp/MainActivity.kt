af6940da51a2ae9acd9d284ba27dd453ba250868
= <=>
2025-01-22 02:25:29 +0530

    initial login successfully working

--- mlbfanengagementapp/app/src/main/java/com/example/fanengagementapp/MainActivity.kt
+++ mlbfanengagementapp/app/src/main/java/com/example/fanengagementapp/MainActivity.kt
@@ -1,321 +1,231 @@
+
 package com.example.fanengagementapp
 
-
-
 import android.annotation.SuppressLint
-
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-
 import androidx.activity.compose.setContent
-
-import androidx.compose.foundation.Image
-
-import androidx.compose.foundation.background
-
 import androidx.compose.foundation.layout.*
-
 import androidx.compose.material3.*
-
 import androidx.compose.runtime.*
-
 import androidx.compose.ui.Alignment
-
 import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.graphics.Color
-
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-
-import androidx.compose.ui.res.painterResource
-
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-
 import androidx.compose.ui.text.style.TextAlign
-
 import androidx.compose.ui.unit.dp
-
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavHostController
-
 import androidx.navigation.compose.*
-import androidx.navigation.compose.rememberNavController
-
 import com.example.fanengagementapp.ui.theme.FanengagementappTheme
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.ktx.Firebase
 
-
-
-
+private const val TAG = "LoginActivity"
 
 class MainActivity : ComponentActivity() {
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
+        //Firebase.initializeApp(this) // Initialize Firebase
         setContent {
-
             FanengagementappTheme {
-
                 Scaffold {
-
                     Surface(
-
                         modifier = Modifier.fillMaxSize(),
-
                         color = MaterialTheme.colorScheme.background
-
                     ) {
-
                         Navigation()
-
                     }
-
                 }
-
             }
-
         }
-
     }
 
 }
 
-
-
 @Composable
-
 fun Navigation() {
-
     val navController = rememberNavController()
-
     NavHost(navController = navController, startDestination = "login_screen") {
-
         composable("login_screen") { LoginScreen(navController) }
-
         composable("signup_screen") { SignupScreen(navController) }
-
+        composable("posts_screen") {
+            // Pass the current activity as an argument to PostsActivity
+            PostsActivity()
+        }
+        composable("user_profile_screen") { UserProfileScreen() }
     }
-
 }
 
-
-
 @Composable
-
 fun LoginScreen(navController: NavHostController) {
-
-    var username by remember { mutableStateOf("") }
-
+    var usernameOrEmail by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-
     val keyboardController = LocalSoftwareKeyboardController.current
-
-
+    val context = LocalContext.current
 
     Column(
-
         modifier = Modifier
-
             .fillMaxSize()
-
             .padding(16.dp),
-
         horizontalAlignment = Alignment.CenterHorizontally,
-
         verticalArrangement = Arrangement.Center
-
     ) {
-
-        Text(text = "MLB Fan Engagement",color = Color.Black, style = MaterialTheme.typography.headlineMedium , textAlign = TextAlign.Center)
-
+        Text(text = "MLB Fan Engagement", color = Color.Black, style = MaterialTheme.typography.headlineMedium, textAlign = TextAlign.Center)
         Spacer(modifier = Modifier.height(16.dp))
 
-
-
         TextField(
-
-            value = username,
-
-            onValueChange = { username = it },
-
+            value = usernameOrEmail,
+            onValueChange = { usernameOrEmail = it },
             label = { Text("Username/Email") },
-
-            modifier = Modifier.fillMaxWidth()
-
+            modifier = Modifier.fillMaxWidth(),
         )
-
         Spacer(modifier = Modifier.height(8.dp))
 
-
-
         TextField(
-
             value = password,
-
             onValueChange = { password = it },
-
             label = { Text("Password") },
-
             modifier = Modifier.fillMaxWidth(),
-
             visualTransformation = PasswordVisualTransformation()
-
         )
-
         Spacer(modifier = Modifier.height(16.dp))
-
-
 
         Button(onClick = {
-
-// Simple login logic (replace with actual authentication)
-
-            if (username == "user" && password == "password") {
-
-// Navigate to a success screen or home screen
-
-// navController.navigate("home_screen")
-
-            } else {
-
-// Show an error message (e.g., Snackbar)
-
-            }
-
-            keyboardController?.hide() // Hide the keyboard after login attempt
-
+            val auth = FirebaseAuth.getInstance()
+            auth.signInWithEmailAndPassword(usernameOrEmail, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Sign-in successful, navigate to home screen
+                        navController.navigate("posts_screen")
+                        Toast.makeText(context, "Login Successful!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        handleFirebaseException(task.exception, context)
+                    }
+                }
+            keyboardController?.hide()
         }) {
-
             Text("Login")
-
         }
-
         Spacer(modifier = Modifier.height(16.dp))
-
-
 
         TextButton(onClick = { navController.navigate("signup_screen") }) {
-
             Text("Don't have an account? Sign Up")
-
         }
-
     }
-
 }
-
-
 
 @Composable
-
 fun SignupScreen(navController: NavHostController) {
-
     var username by remember { mutableStateOf("") }
-
     var email by remember { mutableStateOf("") }
-
     var password by remember { mutableStateOf("") }
-
     val keyboardController = LocalSoftwareKeyboardController.current
-
-
+    val context = LocalContext.current
 
     Column(
-
         modifier = Modifier
-
             .fillMaxSize()
-
             .padding(16.dp),
-
         horizontalAlignment = Alignment.CenterHorizontally,
-
         verticalArrangement = Arrangement.Center
-
     ) {
-
         Text(text = "Sign Up", style = MaterialTheme.typography.headlineMedium)
-
         Spacer(modifier = Modifier.height(16.dp))
 
-
-
         TextField(
-
             value = username,
-
             onValueChange = { username = it },
-
             label = { Text("Username") },
-
             modifier = Modifier.fillMaxWidth()
-
         )
-
         Spacer(modifier = Modifier.height(8.dp))
 
-
-
         TextField(
-
             value = email,
-
             onValueChange = { email = it },
-
             label = { Text("Email") },
-
-            modifier = Modifier.fillMaxWidth()
-
+            modifier = Modifier.fillMaxWidth(),
         )
-
         Spacer(modifier = Modifier.height(8.dp))
 
-
-
         TextField(
-
             value = password,
-
             onValueChange = { password = it },
-
             label = { Text("Password") },
-
             modifier = Modifier.fillMaxWidth(),
-
             visualTransformation = PasswordVisualTransformation()
-
         )
-
         Spacer(modifier = Modifier.height(16.dp))
-
-
 
         Button(onClick = {
+            val auth = FirebaseAuth.getInstance()
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Sign-up successful
+                        navController.navigate("user_profile_screen")
+                        //navController.navigate("posts_screen")
+                        Toast.makeText(context, "Login Successful!", Toast.LENGTH_SHORT).show()
 
-// Handle signup logic here (e.g., API call, data validation)
-
-            keyboardController?.hide() // Hide the keyboard after signup attempt
-
+                    } else {
+                        val intent = Intent(context, PostsActivity::class.java)
+                        context.startActivity(intent)
+                        handleFirebaseException(task.exception, context)
+                    }
+                }
+            keyboardController?.hide()
         }) {
-
             Text("Sign Up")
-
         }
-
         Spacer(modifier = Modifier.height(16.dp))
 
-
-
         TextButton(onClick = { navController.navigate("login_screen") }) {
-
             Text("Already have an account? Login")
-
         }
-
     }
 
 }
 
+@Composable
+fun UserProfileScreen() {
+    val user = FirebaseAuth.getInstance().currentUser
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "User Profile")
+        Spacer(modifier = Modifier.height(16.dp))
+        if (user != null) {
+            Text("Email: ${user.email}")
+            // Display other user information here
+        }
+    }
+}
+
+private fun handleFirebaseException(exception: Exception?, context: Context) {
+    val errorMessage = when (exception) {
+        is FirebaseAuthInvalidCredentialsException -> "Invalid email or password."
+        is FirebaseAuthUserCollisionException -> "Email already in use."
+        is FirebaseAuthWeakPasswordException -> "Password is too weak."
+        else -> "An unknown error occurred."
+    }
+    Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+}
