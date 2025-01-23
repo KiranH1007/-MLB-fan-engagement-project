59e10a74cadbe66cb8a76ef2b9faaa57a9885c95
= <=>
2025-01-23 22:13:46 +0530

    Add model classes for chat, comments, conversations, notifications, posts, stories, users, and user details

---
+++ mlbfanengagementapp/app/src/main/java/com/example/fanengagementapp/models/Comment.kt
@@ -0,0 +1,10 @@
+data class Comment(
+    var commentId: String = "",
+    var comment: String = "",
+    var like_count: String = "",
+    var user_id: String = "",
+    var user_profile_picture: String = "",
+    var time: String = "",
+    var user_name: String = "",
+    var post_id: String = ""
+)
